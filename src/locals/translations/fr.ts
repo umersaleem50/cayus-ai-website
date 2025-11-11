f60e6fe4b23a740b8bb98bb9721135469287c59b
Umar Saleem <umersaleem50@gmail.com>
2025-11-11 14:15:26 +0500

    *fixed fr translation spellings

--- src/locals/translations/fr.ts
+++ src/locals/translations/fr.ts
@@ -1,6 +1,6 @@
 export default {
   'hero.title': 'Liens intelligents avec',
-  'title.highlight': 'Précision',
+  'hero.highlight': 'Précision',
   'hero.para_r1': `Simplifiez la gestion de vos liens avec Cayus.`,
   'hero.para_r2': `Raccourcissez, suivez et organisez tous vos liens en un seul endroit.`,
 } as const;
