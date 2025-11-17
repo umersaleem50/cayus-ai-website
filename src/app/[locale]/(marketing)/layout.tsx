88deb2bb08cdbf603202d3dd117a84d428534cdd
Umar Saleem <umersaleem50@gmail.com>
2025-11-17 20:09:22 +0500

    fix: transparent overlay shift when select toggle

--- src/app/[locale]/(marketing)/layout.tsx
+++ src/app/[locale]/(marketing)/layout.tsx
@@ -1,4 +1,5 @@
 import { Footer, Navbar } from '@/components';
+import GridOverlay from '@/components/global/grid-overlay';
 import React from 'react';
 
 interface Props {
@@ -8,11 +9,7 @@ interface Props {
 const MarketingLayout = ({ children }: Props) => {
   return (
     <>
-      <div
-        id="home"
-        // className="absolute inset-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:3rem_3rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] h-full"
-        className="absolute inset-0 bg-[linear-gradient(to_right,#e5e5e5_1px,transparent_1px),linear-gradient(to_bottom,#e5e5e5_1px,transparent_1px)] bg-[size:3rem_3rem] bg-neutral-100 [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] h-full"
-      />
+      <GridOverlay />
       <Navbar />
       <main className="mt-20 mx-auto w-full z-0 relative">{children}</main>
       <Footer />
---
+++ src/components/global/grid-overlay.tsx
@@ -0,0 +1,11 @@
+function GridOverlay() {
+  return (
+    <div
+      id="home"
+      // className="absolute inset-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:3rem_3rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] h-full"
+      className="fixed inset-0 bg-[linear-gradient(to_right,#e5e5e5_1px,transparent_1px),linear-gradient(to_bottom,#e5e5e5_1px,transparent_1px)] bg-[size:3rem_3rem] bg-neutral-100 [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] h-full"
+    />
+  );
+}
+
+export default GridOverlay;
