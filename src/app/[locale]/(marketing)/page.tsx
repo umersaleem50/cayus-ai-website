dea78f0bb5a1d6990566778c64257658e0824a1e
Umar Saleem <umersaleem50@gmail.com>
2026-01-16 12:17:31 +0500

    *resolved conflicts and rebased

--- src/app/[locale]/(marketing)/page.tsx
+++ src/app/[locale]/(marketing)/page.tsx
@@ -1,11 +1,11 @@
 import AboutUs from '@/components/sections/about-us';
 import CTA from '@/components/sections/cta';
 import Features from '@/components/sections/features';
-<<<<<<< HEAD
+
 import Hero from '@/components/sections/hero';
-=======
+
 import Solution from '@/components/sections/solution';
->>>>>>> ac8762b (+solution cards with avatar group)
+
 import Testimonials from '@/components/sections/testimonials';
 import TheProblem from '@/components/sections/the-problem';
 import TrustedBrands from '@/components/sections/trusted-brands';
@@ -15,14 +15,7 @@ const HomePage = async ({ params }: { params: { locale: string } }) => {
     <div className="overflow-x-hidden scrollbar-hide size-full">
       <Hero />
       <TheProblem />
-<<<<<<< HEAD
-=======
-
-      {/* Solution Section */}
       <Solution />
-
-      {/* Companies Section */}
->>>>>>> ac8762b (+solution cards with avatar group)
       <TrustedBrands />
       <Features />
       <AboutUs />
