862c3c7b0a23adc0772c397ce5ef63bc496c827b
Umar Saleem <umersaleem50@gmail.com>
2026-01-27 11:41:11 +0500

    resolved conflicts for *getStarted.ts in fr translation

--- src/locals/translations/fr/getStarted.ts
+++ src/locals/translations/fr/getStarted.ts
@@ -1,9 +1,7 @@
 export const getStarted = {
   'getStarted.message.error': 'Échec de l’envoi de l’e-mail. Veuillez réessayer plus tard.',
   'getStarted.message.success': 'E-mail envoyé avec succès !',
-<<<<<<< HEAD
+
   'getStarted.cta.title': 'Démarrer',
   'getStarted.input.placeholder': 'votre adresse e-mail',
-=======
->>>>>>> 6de4a03 (refactor translations)
 };
