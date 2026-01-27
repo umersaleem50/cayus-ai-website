dd3eb1bdf49def4030b5a87264f9a56d4f4aa057
Umar Saleem <umersaleem50@gmail.com>
2026-01-27 11:44:24 +0500

    *resolved conflicts for problem translation

--- src/locals/translations/fr/problem.ts
+++ src/locals/translations/fr/problem.ts
@@ -1,6 +1,5 @@
 const problemCards = {
   // Card 1
-<<<<<<< HEAD
   'problem.card.1.title': 'Trop de travail administratif',
   'problem.card.1.description':
     '60 % du temps des managers est consacré à des tâches en dessous de leur niveau de rémunération',
@@ -14,30 +13,13 @@ const problemCards = {
   'problem.card.3.title': 'La surcharge nuit à la rétention',
   'problem.card.3.description':
     'Les talents performants s’épuisent lorsque les promotions arrivent sans accompagnement',
-=======
-  'problem.card.1.title': 'Trop de tâches admin',
-  'problem.card.1.description':
-    '56 % du temps des managers est consacré à l’administratif — pas au leadership.',
-
-  // Card 2
-  'problem.card.2.title': 'La formation ne tient pas',
-  'problem.card.2.description':
-    'Seuls 20 % des formations se traduisent par un changement de comportement.',
-
-  // Card 3
-  'problem.card.3.title': 'La surcharge nuit à la rétention',
-  'problem.card.3.description': 'Quand les managers sont dépassés, la rétention en souffre.',
->>>>>>> 6de4a03 (refactor translations)
 };
 
 export const problem = {
   'problem.badge': 'Le problème',
   'problem.title': 'Le coût caché de la surcharge des managers',
   'problem.subtitle':
-<<<<<<< HEAD
     'Lorsque les managers sont submergés par le travail préparatoire, l’engagement diminue, le turnover augmente et la performance en pâtit.',
-=======
-    'Cayus offre aux RH de la visibilité et du contrôle sur ce qui influence réellement la performance managériale.',
->>>>>>> 6de4a03 (refactor translations)
+
   ...problemCards,
 };
